29495c7d56f6bcba91dc958468f285fe393f2330
Shuflduf <shuflduf@gmail.com>
2026-08-15 19:45:54 -0600

    backpack 2 is done

--- Systems/item_data.gd
+++ Systems/item_data.gd
@@ -3,6 +3,7 @@ extends Resource
 
 @export var display_name: String
 @export var tiles: Array[Vector2i] = []
+@export var sprite: Texture2D
 
 var data: Dictionary[StringName, Variant] = {}
 #
@@ -12,13 +13,15 @@ func build() -> InventoryItem:
 	root.z_index = 2
 	#root.set_anchors_preset(Control.PRESET_CENTER)
 	#root.top_level = true
-	for tile in tiles:
-		var tex = TextureRect.new()
-		tex.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
-		tex.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-		tex.texture = preload("res://icon.svg")
-		tex.size = Vector2(InventoryManager.TILE_SIZE, InventoryManager.TILE_SIZE)
-		tex.position = Vector2(tile) * Vector2(InventoryManager.TILE_SIZE, InventoryManager.TILE_SIZE)
-		root.add_child(tex)
+	for tile_pos in tiles:
+		var tile= Control.new()
+		tile.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+		tile.size = Vector2(InventoryManager.TILE_SIZE, InventoryManager.TILE_SIZE)
+		tile.position = Vector2(tile_pos) * Vector2(InventoryManager.TILE_SIZE, InventoryManager.TILE_SIZE)
+		root.add_child(tile)
+	
+	var tex = TextureRect.new()
+	tex.texture = sprite
+	root.add_child(tex)
 	
 	return root
