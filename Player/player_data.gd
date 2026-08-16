1c15fb749ee7925cfb840baebd56e16968c392e5
Shuflduf <shuflduf@gmail.com>
2026-08-16 02:55:45 -0600

    equipped item display

--- Player/player_data.gd
+++ Player/player_data.gd
@@ -1,4 +1,9 @@
 extends Node
 
+signal equipped_item_changed(new_item: ItemData)
+
 var inventory: Inventory
-var equipped_item: ItemData
+var equipped_item: ItemData:
+	set(new_item):
+		equipped_item = new_item
+		equipped_item_changed.emit(new_item)
--- Player/ui.gd
+++ Player/ui.gd
@@ -4,10 +4,24 @@ extends Control
 @onready var primary_action_label: Label = %PrimaryActionLabel
 @onready var secondary_action_label: Label = %SecondaryActionLabel
 @onready var display_name_label: Label = %DisplayNameLabel
+@onready var held_item: TextureRect = %HeldItem
+
+func _ready() -> void:
+	if PlayerData.equipped_item != null:
+		held_item.texture = PlayerData.equipped_item.sprite
+	PlayerData.equipped_item_changed.connect(_equipped_item_changed)
+
+func _equipped_item_changed(new_item: ItemData) -> void:
+	if new_item != null:
+		held_item.texture = new_item.sprite
+	else:
+		held_item.texture = null
+
 
 func set_display_name_text(new_text: String):
 	display_name_label.visible = new_text != ""
 	display_name_label.text = new_text
+
 
 func set_primary_action_text(new_text: String):
 	primary_action_label.visible = new_text != ""
