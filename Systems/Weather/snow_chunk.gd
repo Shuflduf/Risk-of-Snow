eb4ec56ffb565eafc352b2f76d0b79d78797c772
Shuflduf <shuflduf@gmail.com>
2026-08-18 16:41:40 -0600

    SNOWWWWWWWWWWWW IN RISK OF SNOWWWWWWWWWWWWW

---
+++ Systems/Weather/snow_chunk.gd
@@ -0,0 +1,38 @@
+extends MeshInstance3D
+
+@export var indent_tex: GradientTexture2D
+@export var indent_size: int = 6
+
+@onready var m: PlaneMesh = mesh
+@onready var entity_detector: Area3D = $EntityDetector
+@onready var depth_tex = DrawableTexture2D.new()
+
+var tracked_bodies: Dictionary[Node3D, Vector3]
+
+func _ready() -> void:
+	depth_tex.setup(64, 64, DrawableTexture2D.DRAWABLE_FORMAT_RGBA8, Color.WHITE)
+	m.material.set_shader_parameter(&"snow_depth_texture", depth_tex)
+	entity_detector.body_entered.connect(_on_body_entered)
+	entity_detector.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node3D):
+	tracked_bodies.set(body, body.position)
+	update_snow(body)
+
+func _on_body_exited(body: Node3D):
+	tracked_bodies.erase(body)
+
+func update_snow(body: Node3D):
+	var rel_pos = body.position - position # -16 - 16
+	rel_pos += Vector3(16.0, 0.0, 16.0) # 0 - 32
+	rel_pos *= 2 # 0 - 64
+	print(rel_pos)
+	depth_tex.blit_rect(Rect2i(rel_pos.x - indent_size / 2, rel_pos.z - indent_size / 2, indent_size, indent_size), indent_tex)
+
+
+func _physics_process(_delta: float) -> void:
+	for body: Node3D in tracked_bodies.keys():
+		var old_pos = tracked_bodies[body]
+		if body.position.distance_squared_to(old_pos) > 1.0:
+			tracked_bodies[body] = body.position
+			update_snow(body)
