de6015159753c79c16fdf2ea7ab37635d0e8fcd3
Shuflduf <shuflduf@gmail.com>
2026-08-22 19:09:40 -0600

    the looker

--- Entities/golem.gd
+++ Entities/golem.gd
@@ -1,10 +1,12 @@
 extends CharacterBody3D
 
+const LOOK_MARGIN: float = 1.0
 const SPEED: float = 100.0
 
 var player: CharacterBody3D
 @onready var anim: AnimationPlayer = $Mesh/AnimationPlayer
 @onready var head: MeshInstance3D = $Mesh/Armature/Skeleton3D/Head/Cube
+@onready var max_x_rotation: float = PI / 2.0 - LOOK_MARGIN
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group(&"Player")
@@ -16,9 +18,11 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		
 	if player:
-		
+		var actual_rot: Vector3 = head.rotation
 		var dir: Vector3 = (player.position - position).normalized()
 		head.look_at(player.position)
+		head.rotation.y = lerp_angle(actual_rot.y, head.rotation.y, delta * 2.0)
+		head.rotation.x = clamp(head.rotation.x, -max_x_rotation, max_x_rotation)
 		rotation.y = lerp_angle(rotation.y, atan2(-dir.x, -dir.z), delta * 1.0  ) 
 		velocity.x = dir.x * delta * SPEED
 		velocity.z = dir.z * delta * SPEED
